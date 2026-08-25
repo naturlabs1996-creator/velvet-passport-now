df24062131485964b00971e43fbc21c23021a69e
naturlabs1996-creator <naturlabs1996@gmail.com>
2026-08-25 09:45:36 -0400

    Wire checkout double-click guard into app shell

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import CheckoutClickGuard from "./CheckoutClickGuard";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -40,6 +41,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <CheckoutClickGuard />
         {children}
       </body>
     </html>
