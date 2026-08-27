d6267b3ef3158b1fb16d97b1c6c093f0c86d932d
naturlabs1996-creator <naturlabs1996@gmail.com>
2026-08-27 10:00:36 -0400

    Mount route reliability guard

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from "react";
 import GuardianOfflineFallback from "./GuardianOfflineFallback";
 import RainAheadPrompt from "./RainAheadPrompt";
+import RouteReliabilityGuard from "./RouteReliabilityGuard";
 
 export default function ParisNowLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <GuardianOfflineFallback />
+      <RouteReliabilityGuard />
       <RainAheadPrompt />
       {children}
     </>
